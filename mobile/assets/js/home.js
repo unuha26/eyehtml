a01106435ac9baf9e69db1791540532da87c2d6c
unuha26 <unuha26@users.noreply.github.com>
2017-12-12 16:00:47 +0700

    add youtube detail mobile

--- mobile/assets/js/home.js
+++ mobile/assets/js/home.js
@@ -14,6 +14,14 @@ var close = document.getElementsByClassName("close")[0];
 var menu = document.getElementById('menu');
 var xMenu = document.getElementById("xMenu");
 var xClose = document.getElementsByClassName("xClose")[0];
+var boxNotif = document.getElementsByClassName("display-box-notif")[0];
+var displayNotif = document.getElementsByClassName("box-notif")[0];
+var heighMax = document.getElementsByClassName("deskripsi")[0];
+var heighControl = document.getElementsByClassName("heightControl")[0];
+var heighControl2 = document.getElementsByClassName("heightControl2")[0];
+var xClose2 = document.getElementById("close");
+var containerComment = document.getElementsByClassName("container-comment")[0];
+var openComment = document.getElementsByClassName("openComment")[0];
 src.onclick = function() {
     srcbox.style.display = "block";
 }
@@ -28,6 +36,30 @@ menu.onclick = function() {
 }
 xClose.onclick = function() {
     xMenu.style.display = "none";
+}
+boxNotif.onclick = function() {
+    displayNotif.style.display = "block";
+}
+displayNotif.onclick = function() {
+    displayNotif.style.display = "none";
+}
+heighControl.onclick = function() {
+    heighMax.style.height = "auto";
+    heighControl.style.display = "none";
+    heighControl2.style.display = "block";
+    heighControl2.style.zIndex = "2";
+    heighControl2.style.color = "#000000";
+}
+heighControl2.onclick = function() {
+    heighMax.style.height = "0px";
+    heighControl.style.display = "block";
+    heighControl2.style.display = "none";
+}
+openComment.onclick = function() {
+    containerComment.style.display = "block";
+}
+xClose2.onclick = function() {
+    containerComment.style.display = "none";
 }
 window.onclick = function(event) {
     if (event.target == srcbox) {
